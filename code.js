814e50678175d899f60a7c2d456d3fc5b42e0389
WaffleDevsAlt <81845843+WaffleDevsAlt@users.noreply.github.com>
2022-05-23 20:12:52 -0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,673 @@
+let modmanagerJson = {
+    "curseforge": {
+        "ModMenu": {
+            "slug":"modmenu",
+            "compat": "3"
+         },
+        "Lithium": {
+            "slug": "lithium",
+            "compat": "3"
+        },
+        "LambDynamicLights": {
+            "slug": "lambdynamiclights",
+            "compat": "3"
+        },
+        "Distant Horizons": {
+            "slug": "distant-horizons",
+            "compat": "1"
+        },
+        "Entity Culling": {
+            "slug": "entityculling",
+            "compat": "3"
+        },
+        "Simple Voice Chat": {
+            "slug": "simple-voice-chat",
+            "compat": "1"
+        },
+        "Better Beds": {
+            "slug": "better-beds",
+            "compat": "3"
+        },
+        "FastChest": {
+            "slug": "fastchest",
+            "compat": "3"
+        },
+        "Clumps": {
+            "slug": "clumps",
+            "compat": "3"
+        },
+        "Sodium": {
+            "slug": "sodium",
+            "compat": "1"
+        },
+        "Cull Leaves": {
+            "slug": "cull-leaves",
+            "compat": "3"
+        },
+        "Starlight": {
+            "slug": "starlight",
+            "compat": "3"
+        },
+        "Lazy DFU": {
+            "slug": "lazydfu",
+            "compat": "3"
+        },
+        "Alternate Current": {
+            "slug": "alternate-current",
+            "compat": "3"
+        },
+        "Krypton": {
+            "slug": "krypton",
+            "compat": "3"
+        },
+        "Smoke Suppresssion": {
+            "slug": "smoke-suppression",
+            "compat": "1"
+        },
+        "BetterFPS Render Dis.": {
+            "slug": "better-fps-render-distance",
+            "compat": "1"
+        },
+        "Immersive Portals": {
+            "slug": "immersive-portals-mod",
+            "compat": "1"
+        },
+        "Recipe Cache": {
+            "slug": "recipe-cache",
+            "compat": "3"
+        },
+        "Fabric Language Kotlin": {
+            "slug": "fabric-language-kotlin",
+            "compat": "3"
+        },
+        "Fabric Capes": {
+            "slug": "capes",
+            "compat": "3"
+        },
+        "Wavey Capes": {
+            "slug": "waveycapes",
+            "compat": "1"
+        },
+        "No Weather Effects": {
+            "slug": "no-weather-effects",
+            "compat": "3"
+        },
+        "Lazy Language Loader": {
+            "slug": "lazy-language-loader",
+            "compat": "3"
+        },
+        "Lambda Better Grass": {
+            "slug": "lambdabettergrass",
+            "compat": "3"
+        },
+        "Item Model Fix": {
+            "slug": "item-model-fix",
+            "compat": "3"
+        },
+        "Load My Resources": {
+            "slug": "load-my-resources-fabric",
+            "compat": "3"
+        },
+        "Enhanced Block Entities": {
+            "slug": "enhanced-block-entities",
+            "compat": "3"
+        },
+        "Blur": {
+            "slug": "blur-fabric",
+            "compat": "1"
+        },
+        "Disable Custom World Advice": {
+            "slug": "fabric-disable-custom-worlds-advice",
+            "compat": "3"
+        },
+        "NoFog": {
+            "slug": "nofog",
+            "compat": "3"
+        },
+        "AutoSwitch": {
+            "slug": "fabric-autoswitch",
+            "compat": "3"
+        },
+        "Inventory Sorting": {
+            "slug": "inventory-sorting",
+            "compat": "1"
+        },
+        "Dark Loading Screen": {
+            "slug": "dark-loading-screen",
+            "compat": "2"
+        },
+        "Origins": {
+            "slug": "origins",
+            "compat": "2"
+        },
+        "Physics Mod": {
+            "slug": "physics-mod",
+            "compat": "1"
+        },
+        "RemoveHUDButNotHand": {
+            "slug": "remove-hud-but-not-hand",
+            "compat": "3"
+        },
+        "Skin Layers 3D": {
+            "slug": "skin-layers-3d",
+            "compat": "1"
+        },
+        "Farsight": {
+            "slug": "farsight-fabric",
+            "compat": "2"
+        },
+        "Roughly Enough Items": {
+            "slug": "roughly-enough-items",
+            "compat": "2"
+        },
+        "Applied Energistics 2": {
+            "slug": "applied-energistics-2",
+            "compat": "2"
+        },
+        "Industrial Revolution": {
+            "slug": "industrial-revolution",
+            "compat": "2"
+        },
+        "What The Hell Is This?": {
+            "slug": "wthit",
+            "compat": "3"
+        },
+        "C^2M-Engine": {
+            "slug": "c2me-fabric",
+            "compat": "3"
+        },
+        "Custom Entity Models": {
+            "slug": "custom-entity-models-cem",
+            "compat": "2"
+        },
+        "Animatica": {
+            "slug": "animatica",
+            "compat": "2"
+        },
+        "Ferrite Core": {
+            "slug": "ferritecore-fabric",
+            "compat": "3"
+        },
+        "JMX": {
+            "slug": "jmx",
+            "compat": "3"
+        },
+        "Canvas Renderer": {
+            "slug": "canvas-renderer",
+            "compat": "1"
+        },
+        "Chime": {
+            "slug": "chime-fabric",
+            "compat": "1"
+        },
+        "CIT Resewn": {
+            "slug": "cit-resewn",
+            "compat": "1"
+        },
+        "Colormatic": {
+            "slug": "colormatic",
+            "compat": "1"
+        },
+        "Dynamic FPS": {
+            "slug": "dynamic-fps",
+            "compat": "1"
+        },
+        "Explorer's Compass": {
+            "slug": "explorers-compass",
+            "compat": "3"
+        },
+        "Nature's Compass": {
+            "slug": "natures-compass",
+            "compat": "3"
+        },
+        "Toro's Damage Indicators": {
+            "slug": "torohealth-damage-indicators",
+            "compat": "3"
+        },
+        "Enchantment Descriptions": {
+            "slug": "enchantment-descriptions",
+            "compat": "2"
+        },
+        "Requiem": {
+            "slug": "requiem",
+            "compat": "1"
+        },
+        "Capybara": {
+            "slug": "capybara-fabric",
+            "compat": "3"
+        },
+        "Better End": {
+            "slug": "betterend",
+            "compat": "3"
+        },
+        "Better Nether": {
+            "slug": "betternether",
+            "compat": "3"
+        },
+        "Falling Tree": {
+            "slug": "falling-tree",
+            "compat": "3"
+        },
+        "Appleskin": {
+            "slug": "appleskin",
+            "compat": "3"
+        },
+        "Chisels & Bits": {
+            "slug": "chisels-bits-for-fabric",
+            "compat": "2"
+        },
+        "Xaero's Minimap": {
+            "slug": "xaeros-minimap",
+            "compat": "2"
+        },
+        "Paradise Lost": {
+            "slug": "paradise-lost",
+            "compat": "1"
+        },
+        "Trinkets": {
+            "slug": "trinkets-fabric",
+            "compat": "3"
+        },
+        "Figura": {
+            "slug": "figura",
+            "compat": "2"
+        },
+        "Eating Animation": {
+            "slug": "eating-animation-fabric",
+            "compat": "3"
+        },
+        "Falling Leaves": {
+            "slug": "falling-leaves-fabric",
+            "compat": "3"
+        },
+        "Continuity": {
+            "slug": "continuity",
+            "compat": "3"
+        },
+        "Pling": {
+            "slug": "pling",
+            "compat": "3"
+        },
+        "Sound Physics Remastered": {
+            "slug": "sound-physics-remastered",
+            "compat": "3"
+        },
+        "Boosted Brightness": {
+            "slug": "boosted-brightness",
+            "compat": "3"
+        },
+        "Better Dropped Items": {
+            "slug": "better-dropped-items",
+            "compat": "3"
+        },
+        "Smooth Swapping ": {
+            "slug": "smooth-swapping",
+            "compat": "3"
+        },
+        "Villager Names": {
+            "slug": "villager-names-fabric-version",
+            "compat": "3"
+        },
+        "Custom Stars": {
+            "slug": "custom-stars",
+            "compat": "3"
+        },
+        "Interactic": {
+            "slug": "interactic",
+            "compat": "3"
+        },
+        "StepUpNext": {
+            "slug": "stepupnext",
+            "compat": "3"
+        },
+        "MCA Reborn": {
+            "slug": "minecraft-comes-alive-reborn",
+            "compat": "3"
+        },
+        "Arcanus": {
+            "slug": "arcanus",
+            "compat": "3"
+        },
+        "PlayerEX": {
+            "slug": "playerex",
+            "compat": "3"
+        },
+        "Clean Cut": {
+            "slug": "cleancut",
+            "compat": "2"
+        },
+        "Xaero's World Map": {
+            "slug": "xaeros-world-map",
+            "compat": "2"
+        },
+        "Crimson Chickens": {
+            "slug": "crimson-chickens",
+            "compat": "3"
+        },
+        "Inventory Backpacks": {
+            "slug": "inventory-backpack",
+            "compat": "2"
+        },
+        "Improved Stations": {
+            "slug": "improved-stations",
+            "compat": "3"
+        },
+        "WAIG (Where am i going)": {
+            "slug": "where-am-i-going",
+            "compat": "2"
+        },
+        "Croptopia": {
+            "slug": "croptopia-fabric",
+            "compat": "3"
+        },
+        "IKWID (I know what im doing)": {
+            "slug": "i-know-what-im-doing",
+            "compat": "3"
+        },
+        "Iron Chests": {
+            "slug": "iron-chests-for-fabric",
+            "compat": "2"
+        },
+        "Multiconnect": {
+            "slug": "multiconnect",
+            "compat": "1"
+        },
+        "First Person Model": {
+            "slug": "first-person-model",
+            "compat": "3"
+        },
+        "Not Enough Animations": {
+            "slug": "not-enough-animations",
+            "compat": "3"
+        },
+        "Cloth Config API": {
+            "slug": "cloth-config",
+            "compat": "3"
+        },
+        "Create Refabricated": {
+            "slug": "create-fabric",
+            "compat": "1"
+        },
+        "Moon and Space Dimensions": {
+            "slug": "moon-and-space-dimensions-fabric",
+            "compat": "2"
+        },
+        "Inmis": {
+            "slug": "inmis",
+            "compat": "3"
+        },
+        "ViaFabric": {
+            "slug": "viafabric",
+            "compat": "3"
+        },
+        "Ksyxis": {
+            "slug": "ksyxis",
+            "compat": "3"
+        },
+        "Lucky Blocks for Fabric": {
+            "slug": "luckyblock-fabric",
+            "compat": "3"
+        },
+        "Effective": {
+            "slug": "effective",
+            "compat": "2"
+        },
+        "The Wild": {
+            "slug": "the-wild-mod",
+            "compat": "1"
+        },
+        "Croptopia's Chocolaterie": {
+            "slug": "croptopias-chocolaterie",
+            "compat": "3"
+        },
+        "Detail Armor Bar": {
+            "slug": "detail-armor-bar",
+            "compat": "3"
+        },
+        "True Darkness": {
+            "slug": "true-darkness",
+            "compat": "3"
+        },
+        "Chest Tracker": {
+            "slug": "chest-tracker",
+            "compat": "3"
+        },
+        "MCDoom": {
+            "slug": "mcdoom",
+            "compat": "2"
+        },
+        "Geckolib": {
+            "slug": "geckolib",
+            "compat": "3"
+        },
+        "Patchouli": {
+            "slug": "patchouli",
+            "compat": "3"
+        },
+        "Resource-Overhaul": {
+            "slug": "resource-overhaul",
+            "compat": "3"
+        },
+        "Multi tools": {
+            "slug": "multiple-purpose-tools",
+            "compat": "3"
+        },
+        "Pehkui": {
+            "slug": "pehkui",
+            "compat": "3"
+        },
+        "Replanter": {
+            "slug": "replanter",
+            "compat": "3"
+        },
+        "Better Mount Hud": {
+            "slug": "better-mount-hud",
+            "compat": "3"
+        },
+        "Puzzle": {
+            "slug": "puzzle",
+            "compat": "3"
+        },
+        "Chat Heads": {
+            "slug": "chat-heads",
+            "compat": "3"
+        },
+        "Iris": {
+            "slug": "iris",
+            "compat": "1"
+        },
+        "Iron Furnaces": {
+            "slug": "iron-furnaces-fabric",
+            "compat": "3"
+        },
+        "Trash Slot": {
+            "slug": "trashslot-fabric-edition",
+            "compat": "1"
+        },
+        "Advanced Netherite": {
+            "slug": "advanced-netherite-fabric",
+            "compat": "3"
+        },
+        "Mining Dimensions": {
+            "slug": "mining-dimensions-fabric",
+            "compat": "3"
+        },
+        "Gate Of Babylon": {
+            "slug": "gate-of-babylon",
+            "compat": "3"
+        },
+        "Hookshot": {
+            "slug": "hookshot",
+            "compat": "3"
+        },
+        "FPS - Display": {
+            "slug": "fpsdisplay",
+            "compat": "3"
+        },
+        "Vein Mining": {
+            "slug": "vein-mining-fabric",
+            "compat": "2"
+        },
+        "Eden Ring": {
+            "slug": "eden-ring",
+            "compat": "2"
+        },
+        "When Dungeons Arise!": {
+            "slug": "when-dungeons-arise-fabric",
+            "compat": "3"
+        },
+        "Dragon Loot": {
+            "slug": "dragonloot",
+            "compat": "3"
+        },
+        "Better Sleeping": {
+            "slug": "bettersleeping",
+            "compat": "2"
+        },
+        "Easy Magic Fabric": {
+            "slug": "easy-magic-fabric",
+            "compat": "3"
+        },
+        "Collective": {
+            "slug": "collective-fabric",
+            "compat": "3"
+        },
+        "Friends and Foes": {
+            "slug": "friends-and-foes",
+            "compat": "3"
+        },
+        "Furnace Recycle": {
+            "slug": "furnace-recycle-fabric",
+            "compat": "3"
+        },
+        "Sophisticaed Backpacks": {
+            "slug": "sophisticated-backpacks",
+            "compat": "1"
+        },
+        "Amplified Nether": {
+            "slug": "amplified-nether",
+            "compat": "1"
+        },
+        "BCDT Expansion": {
+            "slug": "bcdt-expansion",
+            "compat": "1"
+        },
+        "Enchanting Infuser": {
+            "slug": "enchanting-infuser-fabric",
+            "compat": "1"
+        },
+        "LevelZ": {
+            "slug": "levelz",
+            "compat": "1"
+        },
+        "Fabric Seasons": {
+            "slug": "fabric-seasons",
+            "compat": "1"
+        },
+        "Gravity Changer": {
+            "slug": "gravitychanger",
+            "compat": "2"
+        },
+        "litematica": {
+            "slug": "litematica",
+            "compat": "2"
+        },
+        "MaLiLib": {
+            "slug": "malilib",
+            "compat": "3"
+        },
+        "Stoneholm": {
+            "slug": "stoneholm",
+            "compat": "3"
+        },
+        "Repurposed Structures": {
+            "slug": "repurposed-structures-fabric",
+            "compat": "3"
+        },
+        "Chunk In A Globe": {
+            "slug": "chunk-in-a-globe",
+            "compat": "1"
+        },
+        "Optifabric/Optifine/Similar": {
+            "slug": "optifabric",
+            "compat": "1"
+        },
+        "Real Poop Mod": {
+            "slug": "minecraft-poop-mod",
+            "compat": "3"
+        },
+        "Transparent": {
+            "slug": "transparent-fabric",
+            "compat": "3"
+        },
+        "Another Simple HUD": {
+            "slug": "ash-another-simple-hud",
+            "compat": "3"
+        },
+        "Phosphor": {
+            "slug": "phosphor",
+            "compat": "2"
+        },
+        "Smooth Boot": {
+            "slug": "smooth-boot",
+            "compat": "2"
+        },
+        "Cull Particles": {
+            "slug": "cull-particles",
+            "compat": "2"
+        },
+        "Bucket Axolotl Fix": {
+            "slug": "axolotl-bucket-fix",
+            "compat": "3"
+        },
+        "Fabric API": {
+            "slug": "fabric-api",
+            "compat": "3"
+        }
+    },
+    "modrinth": {
+        "Fabric Skys": {
+            "slug": "fabricskyboxes",
+            "compat": "3"
+        }
+    }
+}
+let mods = []
+let curseforgeArr = modmanagerJson['curseforge']
+let colors = ['rgb(255,0,0)', 'rgb(200,200,0)', 'rgb(0,255,0)']
+
+for (var a in modmanagerJson.curseforge) {
+    if (modmanagerJson.curseforge.hasOwnProperty(a)) {
+        let mod = modmanagerJson.curseforge[a];
+        mods.push(`${a}:${mod.slug}:${mod.compat}`)
+    }
+}
+mods.sort(function (a, b) {
+    return a.localeCompare(b); //using String.prototype.localCompare()
+});
+for(let i = 0; i < mods.length; i++) {
+    let data = mods[i].split(':')
+    if(data[2] == '3') {
+        $("body").append($(`<div class="modtable"><p id="${data[1]}" class="mod"><a id="${data[1]}l" href="https://www.curseforge.com/minecraft/mc-mods/${data[1]}">${data[0]}</a></div>`));
+        $(`#${data[1]}l`).css('color', colors[data[2]-1])
+        mods.splice(i, 1)
+    }
+}
+
+for(let a = 0; a < mods.length; a++) {
+    let data = mods[a].split(':')
+    if(data[2] == '2') {
+        $("body").append($(`<div class="modtable"><p id="${data[1]}" class="mod"><a id="${data[1]}l" href="https://www.curseforge.com/minecraft/mc-mods/${data[1]}">${data[0]}</a></div>`));
+        $(`#${data[1]}l`).css('color', colors[data[2]-1])
+        mods.splice(a, 1)
+    }
+}
+
+for(let j = 0; j < mods.length; j++) {
+    let data = mods[j].split(':')
+    if(data[2] == '1') {
+        $("body").append($(`<div class="modtable"><p id="${data[1]}" class="mod"><a id="${data[1]}l" href="https://www.curseforge.com/minecraft/mc-mods/${data[1]}">${data[0]}</a></div>`));
+        $(`#${data[1]}l`).css('color', colors[data[2]-1])
+        mods.splice(j, 1)
+    }
+}
